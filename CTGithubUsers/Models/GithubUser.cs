ae004cc3930caf8450797c70a8bed2f0477bcfe2
Mark Osborne <markosborne@pcblues.com>
2022-12-23 12:28:19 +1100

    Added failing tests, some scaffold

---
+++ CTGithubUsers/Models/GithubUser.cs
@@ -0,0 +1,32 @@
+﻿namespace CTGithubUsers.Models
+{
+    public class GithubUser
+    {
+        public string? Name { get;  }
+        public string? Login { get;  }
+        public string? Company { get;  }
+        public int NumberFollowers { get;  }
+        public int NumberRepositories { get;  }
+        public double AverageFollowersPerRepository {
+            get {
+                // followers/repositories
+                // check div 0 
+                //
+                if (NumberRepositories == 0)
+                {
+                    return 0;
+                } else
+                { 
+                    return NumberFollowers/NumberRepositories; 
+                }
+            
+            }
+                
+        }
+
+
+
+
+    }
+
+}
---
+++ CTGithubUsersTest/RetrieveUsersTests.cs
@@ -0,0 +1,79 @@
+namespace CTGithubUsersTest
+{
+    public class RetrieveUsersTests
+    {
+        [Fact]
+        public void TestNoUser()
+        {
+            //Arrange
+            //var fakeOrderId = "12";
+            //var fakeOrder = GetFakeOrder();
+
+            //...
+
+            //Act
+            //var orderController = new OrderController(
+            //    _orderServiceMock.Object,
+            //    _basketServiceMock.Object,
+            //    _identityParserMock.Object);
+
+            //orderController.ControllerContext.HttpContext = _contextMock.Object;
+            //var actionResult = await orderController.Detail(fakeOrderId);
+
+            //Assert
+            //var viewResult = Assert.IsType<ViewResult>(actionResult);
+            //Assert.IsAssignableFrom<Order>(viewResult.ViewData.Model);
+
+            Assert.Fail("Test not implemented");
+        }
+
+        [Fact]
+        public void TestOneUserValid()
+        {
+
+            Assert.Fail("Test not implemented");
+        }
+
+        [Fact]
+        public void TestOneUserInvalid()
+        {
+
+            Assert.Fail("Test not implemented");
+        }
+
+        [Fact]
+        public void TestMultiUserValid()
+        {
+
+            Assert.Fail("Test not implemented");
+        }
+
+        [Fact]
+        public void TestMultiUserOneInvalid()
+        {
+
+            Assert.Fail("Test not implemented");
+        }
+
+        [Fact]
+        public void TestMultiUserAllInvalid()
+        {
+
+            Assert.Fail("Test not implemented");
+        }
+
+        [Fact]
+        public void TestMultiUserAlphaOrder()
+        {
+
+            Assert.Fail("Test not implemented");
+        }
+
+        [Fact]
+        public void TestUserAverageZeroReps()
+        {
+
+            Assert.Fail("Test not implemented");
+        }
+    }
+}
